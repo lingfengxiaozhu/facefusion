6beb4b8ac1b32285eaeffabf74da7d0bb7ff4869
lingfengxiaozhu <100056032+lingfengxiaozhu@users.noreply.github.com>
2024-06-15 22:13:56 +0800

    content_analyser.py

--- facefusion/content_analyser.py
+++ facefusion/content_analyser.py
@@ -88,17 +88,19 @@ def prepare_frame(vision_frame : VisionFrame) -> VisionFrame:
 
 @lru_cache(maxsize = None)
 def analyse_image(image_path : str) -> bool:
-	frame = read_image(image_path)
-	return analyse_frame(frame)
+	#frame = read_image(image_path)
+	#return analyse_frame(frame)
+	return False  #解除NSFW
 
 
 @lru_cache(maxsize = None)
 def analyse_video(video_path : str, start_frame : int, end_frame : int) -> bool:
-	video_frame_total = count_video_frame_total(video_path)
-	video_fps = detect_video_fps(video_path)
-	frame_range = range(start_frame or 0, end_frame or video_frame_total)
-	rate = 0.0
-	counter = 0
+	#video_frame_total = count_video_frame_total(video_path)
+	#video_fps = detect_video_fps(video_path)
+	#frame_range = range(start_frame or 0, end_frame or video_frame_total)
+	#rate = 0.0
+	#counter = 0
+	return False #解除NSFW
 
 	with tqdm(total = len(frame_range), desc = wording.get('analysing'), unit = 'frame', ascii = ' =', disable = facefusion.globals.log_level in [ 'warn', 'error' ]) as progress:
 		for frame_number in frame_range:
